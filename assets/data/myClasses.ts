713028b7cde5d078e885049d3200e0f51a1bfe76
olehv <38218850+olehv@users.noreply.github.com>
2019-07-17 10:39:51 -0400

    Uploaded Angular thirdAppProd Files

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,8 @@
+import { ClassInfo } from "../../app/classinfo";
+
+export const MYCLASSES: ClassInfo[] = 
+[
+    {cname: 'SYST24444', room: 'G407', dow: 'wed', tod: '8-11'},
+    {cname: 'PROG37721', room: 'G407', dow: 'wed', tod: '8-11'},
+    {cname: 'DBAS32100', room: 'G407', dow: 'wed', tod: '8-11'}
+]
